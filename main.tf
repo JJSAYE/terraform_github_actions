051a416b4a1c4502726b6c80c9706908c6d9c3fc
JJSAYE <josephjsaye@gmail.com>
2026-01-06 16:16:30 -0600

    mods

--- main.tf
+++ main.tf
@@ -16,10 +16,10 @@ terraform {
 }
 
 provider "snowflake" {
-  account = var.SNOWFLAKE_ACCOUNT
-  username     = var.SNOWFLAKE_USER
-  password     = var.SNOWFLAKE_PASSWORD
-  role         = var.SNOWFLAKE_ROLE
+  account  = var.SNOWFLAKE_ACCOUNT
+  username = var.SNOWFLAKE_USER
+  password = var.SNOWFLAKE_PASSWORD
+  role     = var.SNOWFLAKE_ROLE
 }
 
 resource "snowflake_database" "demo_db" {
