b6ce7e780b14b1a56a82bfc2cec0b78f3fba1d2b
JJSAYE <josephjsaye@gmail.com>
2026-01-06 15:30:55 -0600

    fixing provider variables

--- main.tf
+++ main.tf
@@ -16,6 +16,10 @@ terraform {
 }
 
 provider "snowflake" {
+  account  = var.SNOWFLAKE_ACCOUNT
+  user     = var.SNOWFLAKE_USER
+  region   = var.SNOWFLAKE_REGION
+  password = var.SNOWFLAKE_PASSWORD
 }
 
 resource "snowflake_database" "demo_db" {
--- variables.tf
+++ variables.tf
@@ -13,11 +13,3 @@ variable "SNOWFLAKE_USER" {
 variable "SNOWFLAKE_PASSWORD" {
   type = string
 }
-
-
-#provider "snowflake" {
-#  account  = var.SNOWFLAKE_ACCOUNT
-#  user     = var.SNOWFLAKE_USER
-#  region   = var.SNOWFLAKE_REGION
-#  password = var.SNOWFLAKE_PASSWORD
-#}
