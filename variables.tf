b05da5df0fea337cfb6bbbd9fe0283f28f73bc33
JJSAYE <josephjsaye@gmail.com>
2026-01-06 14:31:42 -0600

    declaring variables

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "SNOWFLAKE_ACCOUNT" {
+  type = string
+}
+
+variable "SNOWFLAKE_REGION" {
+  type = string
+}
+
+variable "SNOWFLAKE_USER" {
+  type = string
+}
+
+variable "SNOWFLAKE_PASSWORD" {
+  type = string
+}
+
