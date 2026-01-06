86bf6920a51ee8cfc23bfe1d6041fc91e6764674
JJSAYE <josephjsaye@gmail.com>
2026-01-06 15:20:05 -0600

    test

--- variables.tf
+++ variables.tf
@@ -14,9 +14,10 @@ variable "SNOWFLAKE_PASSWORD" {
   type = string
 }
 
+
 provider "snowflake" {
   account = var.SNOWFLAKE_ACCOUNT
   user    = var.SNOWFLAKE_USER
-  region    = var.SNOWFLAKE_REGION
+  region  = var.SNOWFLAKE_REGION
   password = var.SNOWFLAKE_PASSWORD
 }
